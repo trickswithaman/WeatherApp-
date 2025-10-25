f47dad913f943fc348d3c9f34a3c8520d3034e8b
trickswithaman <amansaxenahindu@gmail.com>
2025-10-25 21:38:25 +0530

    test

--- app/src/main/java/com/example/weatherapp/core/utils/RetrofitInstance.kt
+++ app/src/main/java/com/example/weatherapp/core/utils/RetrofitInstance.kt
@@ -7,6 +7,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 
+
 object RetrofitInstance {
     private val loggingInterceptor = HttpLoggingInterceptor().apply {
         level = HttpLoggingInterceptor.Level.BODY
